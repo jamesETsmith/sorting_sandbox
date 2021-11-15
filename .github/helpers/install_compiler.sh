74c2f39311c0ae603a0f5e9976eaeac260e75830
James E T Smith <james.smith9113@gmail.com>
2021-11-15 16:45:36 -0500

    Another typo in CI herlp

--- .github/helpers/install_compiler.sh
+++ .github/helpers/install_compiler.sh
@@ -4,25 +4,25 @@ COMPILER=$1
 
 echo "Trying to install ${COMPILER}"
 
-if [ "${COMPILER}" = "clang++10" ]
+if [ "${COMPILER}" = "clang++-10" ]
 then
     echo "Nothing to install!"
-elif [ "${COMPILER}" = "clang++11" ]
+elif [ "${COMPILER}" = "clang++-11" ]
 then
     echo "Using LLVM script to install clang++11"
     wget https://apt.llvm.org/llvm.sh
     chmod +x llvm.sh
     sudo ./llvm.sh 11
-elif [ "${COMPILER}" = "clang++13" ]
+elif [ "${COMPILER}" = "clang++-13" ]
 then
-    echo "Using LLVM script to install clang++13"
+    echo "Using LLVM script to install clang++-13"
     wget https://apt.llvm.org/llvm.sh
     chmod +x llvm.sh
     sudo ./llvm.sh 13
-elif [ "${COMPILER}" = "g++10" ]
+elif [ "${COMPILER}" = "g++-10" ]
 then
     echo "Nothing to install!"
-elif [ "${COMPILER}" = "g++11" ]
+elif [ "${COMPILER}" = "g++-11" ]
 then
     # https://stackoverflow.com/a/67453352/9371695
     sudo apt install build-essential manpages-dev software-properties-common
