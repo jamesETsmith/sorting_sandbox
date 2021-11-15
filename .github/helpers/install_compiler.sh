78c5604b353b5e093b73d970030523e98da3c7be
James E T Smith <james.smith9113@gmail.com>
2021-11-15 17:06:17 -0500

    Still trying to work out llvm with CI

--- .github/helpers/install_compiler.sh
+++ .github/helpers/install_compiler.sh
@@ -4,8 +4,23 @@ COMPILER=$1
 
 echo "Trying to install ${COMPILER}"
 
-wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
-# Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
+install_llvm () {
+    wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+    # Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
+
+    VERSION=$1
+
+    sudo apt-get install libllvm-${VERSION}-ocaml-dev libllvm${VERSION} llvm-${VERSION} llvm-${VERSION}-dev llvm-${VERSION}-doc llvm-${VERSION}-examples llvm-${VERSION}-runtime
+    sudo apt-get install clang-${VERSION} clang-tools-${VERSION} clang-${VERSION}-doc libclang-common-${VERSION}-dev libclang-${VERSION}-dev libclang1-${VERSION} clang-format-${VERSION} python-clang-${VERSION} clangd-${VERSION}
+    sudo apt-get install libfuzzer-${VERSION}-dev
+    sudo apt-get install lldb-${VERSION}
+    sudo apt-get install lld-${VERSION}
+    sudo apt-get install libc++-${VERSION}-dev libc++abi-${VERSION}-dev
+    sudo apt-get install libomp-${VERSION}-dev
+    sudo apt-get install libclc-${VERSION}-dev
+    sudo apt-get install libunwind-${VERSION}-dev
+}
+
 
 if [ "${COMPILER}" = "clang++-10" ]
 then
@@ -13,17 +28,11 @@ then
 elif [ "${COMPILER}" = "clang++-11" ]
 then
     echo "Using LLVM script to install clang++11"
-    VERSION=11
-    sudo apt-get install clang-${VERSION} libclang-common-${VERSION}-dev\
-        libclang-${VERSION}-dev libclang1-${VERSION} libc++-${VERSION}-dev\
-        libomp-${VERSION}-dev
+    install_llvm "11"
 elif [ "${COMPILER}" = "clang++-13" ]
 then
     echo "Using LLVM script to install clang++-13"
-    VERSION=13
-    sudo apt-get install clang-${VERSION} libclang-common-${VERSION}-dev\
-        libclang-${VERSION}-dev libclang1-${VERSION} libc++-${VERSION}-dev\
-        libomp-${VERSION}-dev
+    install_llvm "13"
 elif [ "${COMPILER}" = "g++-10" ]
 then
     echo "Nothing to install!"
