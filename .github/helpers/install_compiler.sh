ea1352351831296e6de644e3cd7b6152876a1e0a
James E T Smith <james.smith9113@gmail.com>
2021-11-15 16:52:15 -0500

    More CI problems

--- .github/helpers/install_compiler.sh
+++ .github/helpers/install_compiler.sh
@@ -4,21 +4,24 @@ COMPILER=$1
 
 echo "Trying to install ${COMPILER}"
 
+wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key|sudo apt-key add -
+# Fingerprint: 6084 F3CF 814B 57C1 CF12 EFD5 15CF 4D18 AF4F 7421
+
 if [ "${COMPILER}" = "clang++-10" ]
 then
     echo "Nothing to install!"
 elif [ "${COMPILER}" = "clang++-11" ]
 then
     echo "Using LLVM script to install clang++11"
-    wget https://apt.llvm.org/llvm.sh
-    chmod +x llvm.sh
-    sudo ./llvm.sh 11
+    VERSION=11
+    sudo apt-get install clang-${VERSION} libclang-common-${VERSION}-dev libclang-${VERSION}-dev\
+        libc++-${VERSION}-dev libomp-${VERSION}-dev
 elif [ "${COMPILER}" = "clang++-13" ]
 then
     echo "Using LLVM script to install clang++-13"
-    wget https://apt.llvm.org/llvm.sh
-    chmod +x llvm.sh
-    sudo ./llvm.sh 13
+    VERSION=13
+    sudo apt-get install clang-${VERSION} libclang-common-${VERSION}-dev libclang-${VERSION}-dev\
+        libc++-${VERSION}-dev libomp-${VERSION}-dev
 elif [ "${COMPILER}" = "g++-10" ]
 then
     echo "Nothing to install!"
